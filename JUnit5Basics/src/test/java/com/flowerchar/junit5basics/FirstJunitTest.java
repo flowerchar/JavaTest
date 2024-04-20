d74ba2d765afdf6ef28736ed3404d2376ac231cf
unknown <flowerchar2001@gmail.com>
2024-04-20 23:22:45 +0800

    init: init project

---
+++ JUnit5Basics/src/test/java/com/flowerchar/junit5basics/FirstJunitTest.java
@@ -0,0 +1,16 @@
+package com.flowerchar.junit5basics;
+
+import org.junit.jupiter.api.Test;
+
+public class FirstJunitTest {
+
+    @Test
+    void first(){
+        System.out.println("第一个");
+    }
+
+    @Test
+    void second(){
+        System.out.println("第二个");
+    }
+}
